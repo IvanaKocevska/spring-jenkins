7885717455c0f6ca37af1b42466accc26e6da887
ikocevska <kocevska15@gmail.com>
2023-06-17 15:33:26 +0200

    java and sb version

--- src/main/java/com/example/springjenkins/SpringJenkinsApplication.java
+++ src/main/java/com/example/springjenkins/SpringJenkinsApplication.java
@@ -1,10 +1,11 @@
 package com.example.springjenkins;
 
-import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SpringJenkinsApplication {
